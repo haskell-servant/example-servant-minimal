ed03e0059f16e560b1f782e92d73174ecf53b3b2
Sönke Hahn <soenkehahn@gmail.com>
2016-04-05 18:49:23 +0800

    initial commit

---
+++ src/App.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeOperators #-}
+
+module App where
+
+import           Control.Monad.Trans.Except
+import           Data.Aeson
+import           GHC.Generics
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           System.IO
+
+-- * api
+
+type ItemApi =
+  "item" :> Get '[JSON] [Item] :<|>
+  "item" :> Capture "itemId" Integer :> Get '[JSON] Item
+
+itemApi :: Proxy ItemApi
+itemApi = Proxy
+
+-- * app
+
+run :: IO ()
+run = do
+  let port = 3000
+      settings =
+        setPort port $
+        setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $
+        defaultSettings
+  runSettings settings =<< mkApp
+
+mkApp :: IO Application
+mkApp = return $ serve itemApi server
+
+server :: Server ItemApi
+server =
+  getItems :<|>
+  getItemById
+
+type Handler = ExceptT ServantErr IO
+
+getItems :: Handler [Item]
+getItems = return [exampleItem]
+
+getItemById :: Integer -> Handler Item
+getItemById = \ case
+  0 -> return exampleItem
+  _ -> throwE err404
+
+exampleItem :: Item
+exampleItem = Item 0 "example item"
+
+-- * item
+
+data Item
+  = Item {
+    itemId :: Integer,
+    itemText :: String
+  }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON Item
+instance FromJSON Item
+
+data a + b = Foo a b
+
+type X = Int + Bool
