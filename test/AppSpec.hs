954319b102006af7ccc40af990945e15412883b3
Brian Jones <beeceej.code@gmail.com>
2018-11-12 23:46:11 -0600

    un-indent

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -1,43 +1,43 @@
 
 module AppSpec where
 
-  import           Control.Exception (throwIO)
-  import           Network.HTTP.Client (Manager, newManager, defaultManagerSettings)
-  import           Network.HTTP.Types
-  import           Network.Wai (Application)
-  import           Network.Wai.Handler.Warp
-  import           Servant
-  import           Servant.Client
-  import           Test.Hspec
-  import           App hiding (getItems)
+import           Control.Exception (throwIO)
+import           Network.HTTP.Client (Manager, newManager, defaultManagerSettings)
+import           Network.HTTP.Types
+import           Network.Wai (Application)
+import           Network.Wai.Handler.Warp
+import           Servant
+import           Servant.Client
+import           Test.Hspec
+import           App hiding (getItems)
 
-  getItems :: ClientM [Item]
-  getItem :: Integer -> ClientM Item
-  getItems :<|> getItem = client itemApi
+getItems :: ClientM [Item]
+getItem :: Integer -> ClientM Item
+getItems :<|> getItem = client itemApi
 
-  type Host = (Manager, BaseUrl)
+type Host = (Manager, BaseUrl)
 
-  try :: ClientEnv -> ClientM a -> IO a
-  try clientEnv action = either throwIO return =<<
-    runClientM action clientEnv
+try :: ClientEnv -> ClientM a -> IO a
+try clientEnv action = either throwIO return =<<
+  runClientM action clientEnv
 
-  spec :: Spec
-  spec = do
-    describe "/item" $ do
-      withClient mkApp $ do
-        it "lists an example item" $ \ env -> do
-          try env getItems `shouldReturn` [Item 0 "example item"]
-        it "allows to show items by id" $ \ env -> do
-          try env (getItem 0) `shouldReturn` Item 0 "example item"
-        it "throws a 404 for missing items" $ \ env -> do
-          try env (getItem 42) `shouldThrow` anyException
+spec :: Spec
+spec = do
+  describe "/item" $ do
+    withClient mkApp $ do
+      it "lists an example item" $ \ env -> do
+        try env getItems `shouldReturn` [Item 0 "example item"]
+      it "allows to show items by id" $ \ env -> do
+        try env (getItem 0) `shouldReturn` Item 0 "example item"
+      it "throws a 404 for missing items" $ \ env -> do
+        try env (getItem 42) `shouldThrow` anyException
 
 
-  withClient :: IO Application -> SpecWith ClientEnv -> SpecWith ()
-  withClient x innerSpec =
-    beforeAll (newManager defaultManagerSettings) $ do
-      flip aroundWith innerSpec $ \ action -> \ manager -> do
-        testWithApplication x $ \ port -> do
-          let baseUrl = BaseUrl Http "localhost" port ""
-          action (ClientEnv manager baseUrl Nothing)
+withClient :: IO Application -> SpecWith ClientEnv -> SpecWith ()
+withClient x innerSpec =
+  beforeAll (newManager defaultManagerSettings) $ do
+    flip aroundWith innerSpec $ \ action -> \ manager -> do
+      testWithApplication x $ \ port -> do
+        let baseUrl = BaseUrl Http "localhost" port ""
+        action (ClientEnv manager baseUrl Nothing)
 
